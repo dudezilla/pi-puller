5d1296e43e32952560f1223c3aae05f0e799e7d3
dudezilla <29155646+dudezilla@users.noreply.github.com>
2020-10-28 17:24:33 -0600

    working on shell script

--- pull-area/shell-scripting.sh
+++ pull-area/shell-scripting.sh
@@ -1,11 +1,50 @@
-# /bin/sh
+#! /bin/sh
 
-DIRECTORY="TEST-DIR"
+LOGFILE="logfile.txt"
 
-if [ -d "$DIRECTORY" ];
- then
-  # Control will enter here if $DIRECTORY exists.
-  echo "passes."
-fi
+if_dir_exists_do(){
+    #FAIL BY DEFAULT!
+    echo "FROM FUNCTION\n\t $1:"
+    SOMEDIR=$1
+    RETVAL=1
+    if [ -d "$SOMEDIR" ]; then
+    {
+        echo "\t\tdir ["$SOMEDIR"]" >> $LOGFILE
+        RETVAL=0
+    }; else {
+        echo "Warning:\t\tdir ["$SOMEDIR"]\t\t\tdoes not exist" >> $LOGFILE
+        mkdir $SOMEDIR
+        MKDIRRES = $?
+        echo "Resolution:\t\tdir ["$SOMEDIR"]\t\t\t\tCREATED["$MKDIRRES"]" >> $LOGFILE
+    } fi
+    return $RETVAL
+}
 
-echo "huh?"
+# TESTEXPECTED="FAIL"
+# TESTINPUT="BLAH-BLAH"
+# echo "EXPECTED RESULT er["$TESTEXPECTED"] - with input i["$TESTINPUT"]"
+# SOMEVALUE=$(if_dir_exists_do $TESTINPUT) 
+# echo "RET-VALUE WAS:"$SOMEVALUE
+
+# mkdir stuffandjunk
+# TESTEXPECTED="PASS"
+# TESTINPUT="stuffandjunk"
+# echo "EXPECTED RESULT er["$TESTEXPECTED"] - with input i["$TESTINPUT"]"
+# SOMEVALUE=$(if_dir_exists_do $TESTINPUT) 
+# echo "RET-VALUE WAS:"$SOMEVALUE
+# rmdir stuffandjunk
+
+# mkdir stuffandjunk
+# TESTEXPECTED="PASS"
+# TESTINPUT="stuffandjunk"
+# echo "EXPECTED RESULT er["$TESTEXPECTED"] - with input i["$TESTINPUT"]"
+# SOMEVALUE=$(if_dir_exists_do $TESTINPUT) 
+# echo "RET-VALUE WAS:"$SOMEVALUE
+# rmdir stuffandjunk
+
+#TESTEXPECTED="FAIL"
+TESTINPUT="BLAH-BLAH"
+#echo "EXPECTED RESULT er["$TESTEXPECTED"] - with input i["$TESTINPUT"]"
+SOMEVALUE=$(if_dir_exists_do $TESTINPUT) 
+echo "RET-VALUE WAS:"$SOMEVALUE
+
